d86f27006f82f084f278eacc31a4b95001921dc3
Snehil <snehilverma488@gmail.com>
2020-10-21 22:23:57 +0530

    [Snehil] Refactor : Removed Blank Lines

--- FindMaximumUsingGenerics/FindMaximum.cs
+++ FindMaximumUsingGenerics/FindMaximum.cs
@@ -44,12 +44,10 @@ namespace FindMaximumUsingGenerics
             {
                 return firstString;
             }
-
             if (secondString.CompareTo(firstString) > 0 && secondString.CompareTo(thirdString) > 0)
             {
                 return secondString;
             }
-
             if (thirdString.CompareTo(firstString) > 0 && thirdString.CompareTo(secondString) > 0)
             {
                 return thirdString;
